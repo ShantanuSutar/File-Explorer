c7301ad5dea4606a8fe052ec3577019b237aab87
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-04 20:36:16 +0530

    created two buttons to add Folder and File

--- app/src/components/Folder.jsx
+++ app/src/components/Folder.jsx
@@ -2,12 +2,26 @@ import { useState } from "react";
 
 const Folder = ({ explorer }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+
+    setShowInput({ visible: true, isFolder });
+  };
 
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
         <div onClick={() => setExpand(!expand)} className="folder">
           <span>📁 {explorer.name}</span>
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder ➕</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File ➕</button>
+          </div>
         </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
           {explorer.items.map((item) => {
