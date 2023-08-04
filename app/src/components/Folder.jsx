4721114354a5f444a0caa942603aa815b739074e
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-04 20:22:03 +0530

    showing files by clicking on the folder name

--- app/src/components/Folder.jsx
+++ app/src/components/Folder.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 const Folder = ({ explorer }) => {
+  const [expand, setExpand] = useState(false);
+
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
-        <div className="folder">
+        <div onClick={() => setExpand(!expand)} className="folder">
           <span>📁 {explorer.name}</span>
         </div>
-        <div>
+        <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
           {explorer.items.map((item) => {
             return <span key={item.id}>{item.name}</span>;
           })}
